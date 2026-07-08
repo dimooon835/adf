305443ceb473e75cf70c821ea972448715bff3ab
dimooon835 <dimooon835@gmail.com>
2026-07-08 20:41:51 +0300

    Создание блога от 08.07.26

--- users/forms.py
+++ users/forms.py
@@ -9,5 +9,5 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "password2"]
+        fields = ["username", "email", "password1", "password2"]
         
